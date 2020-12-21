b4a1e70dcbc3eb325d10ef559bb886e5727667ee
doowankim <rlaendhks11@gmail.com>
2020-12-21 20:58:45 +0900

    불필요한 스타일코드 삭제

--- src/containers/StartPage/View/styles.tsx
+++ src/containers/StartPage/View/styles.tsx
@@ -1,20 +1,9 @@
 import styled, { css } from 'styled-components';
 import * as color from '../../../colors/index';
-import { darken, lighten } from 'polished';
-
-interface iconProps {
-  light?: boolean;
-}
 
 interface backgroundProps {
   themeMode?: boolean;
 }
-
-export const ButtonBox = styled.div`
-  display: flex;
-  width: max-content;
-  margin: 40px auto;
-`;
 
 export const BodyContainer = styled.div<backgroundProps>`
   height: 100vh;
@@ -56,84 +45,15 @@ export const InputText = styled.div<backgroundProps>`
   }
 `;
 
+export const ButtonBox = styled.div`
+  display: flex;
+  width: max-content;
+  margin: 40px auto;
+`;
+
 export const ModalTitle = styled.div`
   text-align: center;
   margin-bottom: 20px;
-`;
-
-export const IconWrapper = styled.div`
-  display: flex;
-`;
-
-export const IconBox = styled.div<iconProps>`
-  width: 50px;
-  height: 50px;
-  border-radius: 5px;
-  background: black;
-  text-align: center;
-  margin: 0 20px;
-  box-shadow: initial;
-  transition: 0.35s;
-  cursor: pointer;
-
-  &:hover {
-    background: ${lighten(0.05, 'black')};
-    transition: 0.35s;
-  }
-
-  ${props => {
-    if (props.light)
-      return css`
-        background: ${color.Colors.white};
-        border: 1px solid ${color.Colors.line};
-        transition: 0.35s;
-
-        &:hover {
-          background: ${darken(0.05, `${color.Colors.white}`)};
-          transition: 0.35s;
-        }
-      `;
-  }}
-`;
-
-export const Icon = styled.i<iconProps>`
-  color: ${color.Colors.white};
-  line-height: 50px;
-
-  ${props => {
-    if (props.light)
-      return css`
-        color: black;
-      `;
-  }}
-`;
-
-export const IconText = styled.div`
-  display: flex;
-  width: 148px;
-  font-size: 14px;
-  font-weight: bold;
-  justify-content: space-between;
-  margin: 10px 15px 30px 15px;
-`;
-
-export const Button = styled.button`
-  width: 180px;
-  border: 1px solid ${color.Colors.line};
-  border-radius: 5px;
-  padding: 10px 0;
-  cursor: pointer;
-  background: ${color.Colors.background};
-  transition: 0.35s;
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover {
-    background: ${darken(0.05, `${color.Colors.background}`)};
-    transition: 0.35s;
-  }
 `;
 
 export const InputBox = styled.div`
